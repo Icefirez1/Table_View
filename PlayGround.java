7a20466a579bfb055ac4adc5ca10797bc5e05414
Al <alpagar9@gmail.com>
2022-04-06 15:44:51 -0400

    completed base table

--- PlayGround.java
+++ PlayGround.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+
 import javafx.application.Application;
+import javafx.beans.binding.ObjectExpression;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -9,6 +12,9 @@ import javafx.stage.Stage;
 
 public class PlayGround extends Application
 {
+    private ArrayList<String> fNames;
+    private ArrayList<String> lNames; 
+    private TableView tableView = new TableView<>();
     /**
      * Constructor with constant variables
      */
@@ -28,10 +34,26 @@ public class PlayGround extends Application
      * Application starting
      */
     @Override
+    @SuppressWarnings("unchecked")
     public void start(Stage primary)
     {
-        TableView tableView = new TableView<>();
         
+        TableColumn<Person, String> column1 = new TableColumn<>("First Name");
+        column1.setCellValueFactory(
+            new PropertyValueFactory<>("firstName"));
+        TableColumn<Person, String> column2 = new TableColumn<>("Last Name");
+        column2.setCellValueFactory(
+                new PropertyValueFactory<>("lastName"));
+
+
+        tableView.getColumns().add(column1);
+        tableView.getColumns().add(column2);
+
+
+        tableView.getItems().add(new Person("Eric", "Chen"));
+        tableView.getItems().add(new Person("Ivan", "Zheng"));
+        tableView.getItems().add(new Person("Your", "Mom"));
+
 
 
         //adding and running program
@@ -41,11 +63,71 @@ public class PlayGround extends Application
         primary.show();
     }
     /**
+     * will add people to the thingy
+     */
+    
+    public void addPeople()
+    {
+        //toDO: making input function to add people
+        //textBox?
+        //Scanner? 
+    }
+    /**
      * stopping application
      */
     @Override 
     public void stop()
     {
 
+    }
+
+    /*Person subclass*/
+    public class Person 
+    {
+        public String firstName; 
+        public String lastName; 
+
+        /**
+         * person constructor
+         * @param firstName
+         * @param lastName
+         */
+        public Person(String firstName, String lastName)
+        {
+            this.firstName = firstName; 
+            this.lastName = lastName; 
+        }
+        /**
+         * returns first name
+         * @return First name
+         */
+        public String getFirstName()
+        {
+            return firstName;
+        }
+        /**
+         * sets First name
+         * @param newName
+         */
+        public void setFirstName(String newName)
+        {
+            this.firstName = newName; 
+        }
+        /**
+         * returns last name
+         * @return lastName
+         */
+        public String getLastName()
+        {
+            return lastName;
+        }
+        /**
+         * sets Last name
+         * @param newName
+         */
+        public void setLastName(String newName)
+        {
+            this.lastName = newName; 
+        }
     }
 }
